9b7412c7784989eb75979d53293f9ad778016330
zarateganso10 <matheuszarate55@gmail.com>
2022-06-25 18:18:08 -0300

    app

---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import { Event } from "./pages/Event";
+import { Subscribe } from "./pages/Subscribre";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Subscribe />} />
+      <Route path="/event" element={<Event />} />
+      <Route path="/event/lesson/:slug" element={<Event />} />
+    </Routes>
+  )
+}
